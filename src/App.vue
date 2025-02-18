723086d65126e12c8fa48b6154eab483b19aea4a
Nikita <nikita.gonohov.it@gmail.com>
2025-02-18 23:27:58 +0200

    initial tailwind

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,6 @@ import NavBar from "./components/NavBar.vue";
 <template>
     <div>
         <NavBar />
-        <div class="p-6 bg-blue-500 text-white">
-            <h1 class="text-2xl font-bold">Тест Tailwind CSS</h1>
-            <p class="mt-2">
-                Если этот текст белый на синем фоне, Tailwind работает!
-            </p>
-        </div>
         <router-view />
     </div>
 </template>
